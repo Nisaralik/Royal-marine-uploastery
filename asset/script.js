ca5ebf29c376f4b5d9bdbca845f7260288395c6c
nizarali <nizareali007@gmal.com>
2024-07-26 09:22:28 +0530

    new

---
+++ asset/script.js
@@ -0,0 +1,15 @@
+window.addEventListener("scroll", function () {
+    var header = this.document.querySelector("header");
+    header.classList.toggle("sticky", this.window.scrollY > 0);
+})
+const navMenu = document.getElementById("nav-menu");
+const toggleMenu = document.getElementById("toggle-menu");
+const closeMenu = document.getElementById("close-menu");
+toggleMenu.addEventListener('click', () => {
+    navMenu.classList.toggle('show');
+    document.body.classList.toggle('overflow-hidden');
+});
+closeMenu.addEventListener("click", () => {
+    navMenu.classList.remove('show');
+    document.body.classList.remove('overflow-hidden');
+});
